91fe7768bace50079c2fbe7ca4a54d0230271a6b
adeneye02 <tosinolatunbosun02@gmail.com>
2023-05-28 16:17:07 +0100

    new commit

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,90 +1,73 @@
 #!/usr/bin/python3
-"""route for handling User objects and operations"""
-
+""" View for User objects that handles default API actions """
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
 from models import storage
 from models.user import User
+import hashlib
 
 
-@app_views.route("/users")
-def all_users():
-    '''retrieves all User objects
-    :return: json of all users'''
-
-    user_list = []
-    for user in storage.all("User").values():
-        user_list.append(user.to_dict())
-    return jsonify(user_list)
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def users():
+    """ Retrieves the list of all User objects """
+    d_users = storage.all(User)
+    return jsonify([obj.to_dict() for obj in d_users.values()])
 
 
-@app_views.route("/users/<user_id>")
-def user(user_id):
-    '''gets a specific User object by ID
-    :param user_id: user object id
-    :return: user obj with the specified id or error'''
-
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def r_user_id(user_id):
+    """
+    file: yml/users_get.yml
+    """
     user = storage.get("User", user_id)
     if not user:
         abort(404)
     return jsonify(user.to_dict())
 
 
-@app_views.route("/users/<user_id>", methods=['DELETE'])
-def delete_user(user_id):
-    '''deletes User by id
-    :param user_id: user object id
-    :return: empty dict with 200 or 404 if not found'''
-
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def del_user(user_id):
+    """ Deletes a User object """
     user = storage.get("User", user_id)
     if not user:
         abort(404)
-    storage.delete(user)
+    user.delete()
     storage.save()
     return make_response(jsonify({}), 200)
 
 
-@app_views.route("/users", methods=["POST"])
-def create_user():
-    '''create user route
-    :return: newly created user obj'''
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def post_user():
+    """ Creates a User object """
+    new_user = request.get_json()
+    if not new_user:
+        abort(400, "Not a JSON")
+    if "email" not in new_user:
+        abort(400, "Missing email")
+    if "password" not in new_user:
+        abort(400, "Missing password")
 
-    if not request.get_json():
-        abort(400, description="Not a JSON")
-
-    if not request.get_json().get('email'):
-        abort(400, description="Missing email")
-
-    if not request.get_json().get('password'):
-        abort(400, description="Missing password")
-
-    user = User()
-    user.email = request.get_json()['email']
-    user.password = request.get_json()['password']
-    user.save()
-
+    user = User(**new_user)
+    storage.new(user)
+    storage.save()
     return make_response(jsonify(user.to_dict()), 201)
 
 
-@app_views.route("/users/<user_id>", methods=['PUT'])
-def update_user(user_id):
-    '''updates specific User object by ID
-    :param user_id: user object ID
-    :return: user object and 200 on success, or 400 or 404 on failure'''
-
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def put_user(user_id):
+    """ Updates a User object """
     user = storage.get("User", user_id)
     if not user:
         abort(404)
 
-    if not request.get_json():
-        abort(400, description="Not a JSON")
+    body_request = request.get_json()
+    if not body_request:
+        abort(400, "Not a JSON")
 
-    for key, val in request.get_json().items():
-        if key == "id" or key == "created_at" or key == "updated_at" or key == "email":
-            continue
-        else:
-            setattr(user, key, val)
+    for k, v in body_request.items():
+        if k not in ['id', 'email', 'created_at', 'updated_at']:
+            setattr(user, k, v)
 
     storage.save()
-
     return make_response(jsonify(user.to_dict()), 200)
